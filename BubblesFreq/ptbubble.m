9d6696bd8d2c5abeea2b7e38869c1075c9f8989e
Lisa Schwetlick <lisaschwetlick@gmail.com>
2015-11-15 17:48:58 +0100

    implemented clicking for answers and checking for correctness#

--- BubblesFreq/ptbubble.m
+++ BubblesFreq/ptbubble.m
@@ -2,7 +2,11 @@
 %clear workspace
 clear
 
-%addpath('C:\Users\labor\Documents\Forschung\Bubbles\Functions')
+addpath('./Functions')
+addpath('./Data')
+addpath('./Settings')
+addpath('./Stimuli')
+
 %% Set up Data
 subject = input('VP Nummer:', 's');
 bubbleFileName = strcat('Data/Bubbles__Subject',subject,'_Coordinates.csv');
@@ -22,58 +26,22 @@ postFileHandle = fopen(postFileName, 'w');
 fprintf(postFileHandle, 'percent correct at the end\n');
 
 %% Loading in the Stimulus-Images
-dirName = {'./Stimuli/subset1', './Stimuli/subset2','./Stimuli/subset3','./Stimuli/subset4'};
-allData=LoadImgData(dirName, subject);
-dataCopy=allData;
-%adding all targets and pretest-distractors
-pretestData=allData([allData.type]==1 | [allData.type]==3);
-%adding all targets and distractors
-data=allData([allData.type]==1 | [allData.type]==4);
-%posttest data
-posttestData=allData([allData.type]==1 | [allData.type]==2);
+dirName = {'./Stimuli/imgs/jpg'};
+data=LoadImgData(dirName);
+dataCopy=data;
+
 %% bubble parameters
-load('Settings/BubbleSettings.mat')
+load('Settings/BubbleSettings.mat');
 % settings has the following variables:
 % - sd
 % - shadeOfGrey
 % - background
-
-%amounts = settings.amounts;
-amounts = [20];
-nAmounts = numel(amounts);
-nTargets = sum([data.type]==1);
-nDistractors = sum([data.type]==4);
-% nEach = settings.nTimes;
-nEach = 1;
-
-amountList = repmat(amounts,1,(nTargets*2*nEach));
-nPerImage = nAmounts*nEach;
-%%%%CHECK THIS AGAIN!!!
-totalTrials = nAmounts*nTargets*2*nEach;
-
-%Image List
-targetList = repmat([1:nTargets],1,nPerImage);
-% % Amount of distractors given by the Ratio
-neededD= length(targetList)*2*settings.distractorRatio;
-neededDFactor= neededD/nDistractors;
-distractorList = repmat([nTargets+1:nTargets+nDistractors],1,ceil(neededDFactor));
-distRemove= nDistractors*(ceil(neededDFactor)-neededDFactor);
-distractorList(length(distractorList)-distRemove+1:length(distractorList))=[];
-% %concatenating to become 
-imageIndexList=[targetList,distractorList];
-% 
-if numel(imageIndexList)~=totalTrials
-    error('Error: Number of trials and number of Images need to be equal')
-end
-% % the image list gets sorted to be [1,1,1,1,2,2,2,2,3,3,3,3]
-imageIndexList=sort(imageIndexList);
-% % the amounts list stays [1,2,3,4,1,2,3,4]
-trialList = struct('imageIndex',num2cell(imageIndexList),'nBubbles',num2cell(amountList));
+nImages= numel(data);
+nEach=1;
+nTrials=nImages*nEach;
 
 %% setting up presentation
 %Debugging mode: depending on monitor setup
-%% setting up presentation
-%depending on monitor setup
 Screen('Preference', 'SkipSyncTests', 1); 
 if length(Screen('screens'))==1
     monitor= 0;
@@ -98,44 +66,35 @@ end
 
 [windowWidth, windowHeight]=Screen('WindowSize', window);
 %% First Image viewing
-FirstInstructions(window)
-WaitSecs(1);
-RestrictKeysForKbCheck([]);
-KbWait();
-TargetsAgain(window, pretestData)
+% FirstInstructions(window)
+% WaitSecs(1);
+% RestrictKeysForKbCheck([]);
+% KbWait();
+% TargetsAgain(window, pretestData)
 %% Pretest
 %Pretest(window, pretestData, settings)
 %% Presentation of main instructions
-RestrictKeysForKbCheck([]);
-InstructionsMain(window)
-WaitSecs(1);
-KbWait();
+% RestrictKeysForKbCheck([]);
+% InstructionsMain(window)
+% WaitSecs(1);
+% KbWait();
 %% Stimulus presentation
 feedbackCounter=0;
-for trial=1:(totalTrials)
+for trial=1:(nTrials)
+    %every 50th trial there is a Break with feedback
+    RestrictKeysForKbCheck([ ]);
     if mod(trial,50)==0
         Pause(window, feedbackCounter)
-        
         WaitSecs(1);
         RestrictKeysForKbCheck([]);
         KbWait();
         feedbackCounter=0;
     end
     
-     randIndex = randi(numel(trialList));
-     imno = trialList(randIndex).imageIndex;
-     nBubbles = trialList(randIndex).nBubbles;
-     trialList(randIndex)=[];
+    imno = randi([1, numel(data)]);
+    disp('Targetnr')
+    disp(data(imno).id)
     
-    %resize
-%     [imSizeY,imSizeX, unused] = size(data(imno).image);
-%     x=windowWidth/imSizeX;
-%     y= windowHeight/imSizeY;
-%     f = min([x,y]);
-    
-    %instance of class
-%     smallImage=imresize(data(imno).image,f-0.01);
-
     %present FixCross
     big=50;
     line=5;
@@ -144,9 +103,9 @@ for trial=1:(totalTrials)
     fixT=GetSecs();
 %     WaitSecs(1);
     
-    smallImage=data(trial).image;
+    smallImage=data(imno).image;
     
-    stim=bubbles(smallImage, 20,12);
+    stim=bubbles(rgb2gray(smallImage));
     theImage=stim.stimulus();
 %     disp(amountList(amno))
 %     disp(data(imno).type)
@@ -161,54 +120,77 @@ for trial=1:(totalTrials)
         
     %this prepares the image for PTB presentation
     imageTexture = Screen('MakeTexture', window, theImage);
-    %make it wait longer if the calculation was quick
-    fixT2 = GetSecs();
-    if fixT2-fixT<1
-        WaitSecs((1-(fixT2-fixT)));
-%         disp('i had to wait')
-%         disp(1-(fixT2-fixT))
+    
+    boxStats=OptBoxMaker(nImages, windowHeight, windowWidth);
+%make colors
+    for i =[1:nImages]
+        if mod(i,2)==0
+            cols(1:3,i)=100;
+        else
+            cols(1:3,i)=200;
+        end
     end
+
+    Screen('TextFont', window, 'Arial');
+    Screen('TextSize', window, 30);
+    Screen('TextStyle', window, [0]);
+    textCol=[0 0 0];
+    x=1;
+    SetMouse(0, 0, window);
+    while x < windowWidth-200
+        disp('FUUU')
+        
+        for i=[1:nImages]
+            Screen('FillRect', window, cols ,boxStats)
+        end
+        for i=[1:nImages]
+            Screen('DrawText', window, dataCopy(i).name, boxStats(1,i)+30, boxStats(2,i)+15,textCol);
+        end
+        Screen('DrawTexture', window, imageTexture, [], [], 0);
+        Screen('Flip', window);
+        [clicks,x,y,whichButton] = GetClicks(window);
+
+    end
+
+%outputs the answer given
+    for i=[1:nImages]
+        if y<boxStats(4,i)
+            ans=i;
+            disp('ans')
+            disp(i)
+            break
+        end
+    end
+    if ans==data(imno).id
+        correct=true;
+    else
+        correct=false;
+    end
+    disp('correct')
+    disp(correct)
     
-    Screen('DrawTexture', window, imageTexture, [], [], 0);
-    %this presents the prepared bit in the window
-    
-    t0=Screen('Flip', window);
-    WaitSecs(0.1);
-    
-    %guard against other keys being pressed. Program will not react to keys
-    %other than 'y' and 'n'
-    RestrictKeysForKbCheck([KbName('y'), KbName('n')]);
-    [t1, keyCode, deltaSecs]=KbWait();
-    
-    secs=t1-t0;
-   
-     if KbName(keyCode)=='y'
-         if data(imno).type==1
-             correct=true;
-         else
-             correct=false;
-         end
-         
-     elseif KbName(keyCode)=='n'
-         if data(imno).type==1
-             
-             correct=false;
-         else
-             correct=true;
-         end
-     end
-%     
-     if correct==true
-         feedbackCounter=feedbackCounter+1;
-     end
-    %'Trial, Image, Amount, Target, Response, Correct, Time'
-     fprintf(infoFileHandle, '%s,%d,%s,%f,%d,%s,%d,%f\n', subject, trial, data(imno).name, nBubbles, data(imno).type, KbName(keyCode), double(correct), secs);
-%     
-%     data(imno).seen = data(imno).seen+1;
-%     if data(imno).seen==nPerImage && data(inmo).type~=4        
-%     %delete from list, so that it doesnt show up again
-%         data(imno)=[];
+%     %make it wait longer if the calculation was quick
+%     fixT2 = GetSecs();
+%     if fixT2-fixT<1
+%         WaitSecs((1-(fixT2-fixT)));
+% %         disp('i had to wait')
+% %         disp(1-(fixT2-fixT))
 %     end
+
+%     secs=t1-t0;
+%    
+% %     
+%      if correct==true
+%          feedbackCounter=feedbackCounter+1;
+%      end
+    %'Trial, Image, Amount, Target, Response, Correct, Time'
+%      fprintf(infoFileHandle, '%s,%d,%s,%f,%d,%s,%d,%f\n', subject, trial, data(imno).name, nBubbles, data(imno).type, KbName(keyCode), double(correct), secs);
+     
+    data(imno).seen = data(imno).seen+1;
+    if data(imno).seen==nEach
+    %delete from list, so that it doesnt show up again
+        data(imno)=[];
+    end
 %     amountList(amno)=[];
     Screen('Close', imageTexture)
     
@@ -220,11 +202,11 @@ end
 % KbWait()
 % Posttest(window, posttestData, settings, postFileHandle)
 
-%% TY
-ThankYou(window)
-RestrictKeysForKbCheck([]);
-WaitSecs(0.3);
-KbWait()
+% %% TY
+% ThankYou(window)
+% RestrictKeysForKbCheck([]);
+% WaitSecs(0.3);
+% KbWait()
 
 %% Close Everything
 fclose(bubbleFileHandle);
