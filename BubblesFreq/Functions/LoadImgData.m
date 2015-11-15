2b7fd4fa197a68063b5d594e70444cf3177edcb6
Lisa Schwetlick <lisaschwetlick@gmail.com>
2015-11-15 17:43:01 +0100

    LoadImgData now outputs a struct of stimulus images, no distractors and including names

--- BubblesFreq/Functions/LoadImgData.m
+++ BubblesFreq/Functions/LoadImgData.m
@@ -1,62 +1,33 @@
-function [ data ] = LoadImgData( dirName , subject)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function [ data ] = LoadImgData( dirName )
+%This function loads a folder of images into the program. It takes the
+%directory path as an input and gives a struct with the image, file name and
+%file path
 data=struct;
+
 for i=1:numel(dirName) %over all folders
     files={};
     files = dir( fullfile(dirName{i},'*.jpg') );
     files = {files.name}';
     
     for j=1:numel(files)
-        fname = fullfile(dirName{i},files{j});     %# full path to file
+        fpath = fullfile(dirName{i},files{j});     %# full path to file
+        fname = files{j};
         
-        n=numel(data)+1; %we store it in the "data" struct
-        if i==1 && j==1 % for the first runthrough n needs to be 1 (not two as it would otherwise be)
-            n=1;
-        end %if
+        
         %transform to black and white
-        data(n).image = (imread(fname)); % load file
-        data(n).name = fname; %store file name
+        data(j).image = (imread(fpath)); % load file
+        data(j).name = fname(1:end-4); %file name
+        data(j).path = fpath; %store file path
+        data(j).seen = 0;
+        data(j).id = j;
+        
                 
-        %store type           
-        % even vp numbers get set 1 as target and set 2 as distractors
-        % 1 is a target, 2 a secondary target, 3 a predistractor, 4 distractor
-        if i==1
-            data(n).type=1;
-        elseif i==2
-            data(n).type=2;
-        elseif i==3
-            data(n).type=3;
-        else
-            data(n).type=4;
-        end %if 2
         
-        
-%         if mod(str2num(subject),2)==0
-%             if i==1
-%                 data(n).type=1;
-%             elseif i==2
-%                 data(n).type=2;
-%             elseif i==3
-%                 data(n).type=3;
-%             else
-%                 data(n).type=4;
-%             end %if 2
-%         % odd vp numbers get set 2 as target and set 1 as distractors
-%         else
-%             if i==1
-%                 data(n).type=2;
-%             elseif i==2
-%                 data(n).type=1;
-%             elseif i==3
-%                 data(n).type=3;
-%             else
-%                 data(n).type=4;
-%             end %if 2
-%         end
                
     end % for 2
 end %for 1
+
+clearvars files fname fpath dirName i j
 
 end
 
